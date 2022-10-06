7d21527b7f58d7bcf3309b528b6022556cc4245b
tuananhcu <tuananhcu2411@gmail.com>
2022-10-06 19:55:06 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "Faded",
+    artist: "Alan Walker",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Rather Be",
+    artist: "Clean Bandit",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Stay",
+    artist: "Justin Bieber ",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Falling down",
+    artist: "Lil Peep & XXXTENTACTION",
+    img: "music-4",
+    src: "music-4",
+  },
+];
